41701af6db69eb9b185d9947d6a310f10b248ba1
Jaey <jaeyoung.kim@student.manchester.ac.uk>
2023-03-07 22:46:26 +0000

    Programmers java

---
+++ java/ReversingArray/ReversingArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ReversingArray {
+  public static void main(String[] args) {
+    int[] num_list = { 1, 2, 3, 4, 5 };
+    int[] answer = new int[num_list.length];
+    for (int i = 0; i < num_list.length; i++) {
+      answer[i] = num_list[num_list.length - i - 1];
+    }
+    System.out.println(Arrays.toString(answer));
+  }
+}
