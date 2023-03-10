a86d876c02b360dde04a3a868e2c9cebcafe2bf9
Jaey <jaeyoung.kim@student.manchester.ac.uk>
2023-03-10 20:26:55 +0000

    Ice Americano

---
+++ java/IceAmericano/IceAmericano.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class IceAmericano {
+  public static void main(String[] args) {
+    int[] num_list = { 15000 / 5500, 15000 % 5500 };
+    System.out.println(Arrays.toString(num_list));
+  }
+
+}
